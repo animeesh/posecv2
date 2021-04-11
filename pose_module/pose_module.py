ce19913ecfcb1fe979a436d97e1d48ccb58f402b
Animeshkumar Nayak <36356111+animeesh@users.noreply.github.com>
2021-04-11 11:44:22 +0530

    Add files via upload

--- pose_module/pose_module.py
+++ pose_module/pose_module.py
@@ -46,10 +46,10 @@ class poseDetector():
         x2,y2=self.lmlist[p2][1:]
         x3,y3=self.lmlist[p3][1:]
         #get the Angles
-        angle=math.degrees(math.atan2(y3-y2,x3-x2)-math.atan2(y1-y2,x3-x2))
-        #print(angle)
+        angle=math.degrees(math.atan2(y3-y2,x3-x2)-math.atan2(y1-y2,x1-x2))
         if angle <0:
             angle += 360
+         
         cv2.putText(img,str(int(angle)),(x2-50,y2+20),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),3)
 
 
@@ -78,12 +78,12 @@ def main():
     while True:
         success,img=cap.read()
         img=cv2.resize(img,(1200,720))
-        img=detector.findpose(img)
-        lmlist=detector.getpos(img,Draw=True)
+        img=detector.findpose(img,False)
+        lmlist=detector.getpos(img,Draw=False)
 
         if len(lmlist)!=0:
         #right arm
-        #detector.findangle(img,12,14,16)
+            detector.findangle(img,16,14,12)
         #left arm
             detector.findangle(img,11,13,15)
 
